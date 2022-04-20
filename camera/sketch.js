95f1c54dc757eb2cb1876d48d554e931b68d7392
Jens1804 <jens_dalmus@yahoo.de>
2022-04-20 11:31:40 +0200

    resolve sonar cloud error

--- camera/sketch.js
+++ camera/sketch.js
@@ -60,12 +60,6 @@ function draw() {
       const len = density.length;
       const charIndex = floor(map(avg, 0, 255, len, 0));
       const bla = 1;
-      function blub5(a, b, c, d, e, f, g, h, i, j) {
-        return "hihi";
-      }
-      function blub7(a, b, c, d, e, f, g, h, i, j) {
-        return "hihi";
-      }
 
       textSize(w);
 
